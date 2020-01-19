a13b4cad67dce05bc37d50f52ef6b9554e80c1fd
ThuglifeScientist2018 <=>
2020-01-19 08:28:44 +0545

    commit 2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 var coin = document.querySelector("#coin");
+var p1display = document.querySelector("#p1display");
+p1display.textContent = p1score;
+var p2display = document.querySelector("#p2display");
+p2display.textContent = p2score;
 var coinicon = document.querySelector("i");
-var inputplayers = document.querySelector("#pinput");
+var resetButton = document.querySelector("#reset");
+var winScore = 5;
 var icons = ["fa fa-tablet", "fa fa-desktop"];
 var p1score = 0;
 var p2score = 0;
-var p1name;
-var p2name;
+var gameover = false;
+
+
+if(gameover !== true){
 
 coin.addEventListener("click", function(){
 
@@ -18,18 +25,43 @@ coin.addEventListener("dblclick", function(){
   coinicon.setAttribute("class", icons[randicon]);
   var coiniconselect = coinicon.getAttribute("class");
   if(coiniconselect === icons[0]) {
+    if(p1score === winScore || p2score === winScore){
+      console.log("Game Over");
+      gameover = true;
+    }else{
     p1score ++;
+
     p1display.textContent = p1score;
   }
+  }
   else if(coiniconselect === icons[1]) {
+    if(p1score === winScore || p2score === winScore){
+      console.log("Game Over");
+      gameover = true;
+
+
+    }else {
     p2score ++;
+
+
     p2display.textContent = p2score;
   }
+  }
+
 })
 
-inputplayers.addEventListener("keypress", function(key){
-  if(key.KeyCode === 13){
-    p1name = this.value;
-  }
+
+}
+
+
+
+
+
+resetButton.addEventListener("click", function(){
+  coinicon.removeAttribute("class");
+  p1score = 0;
+  p2score = 0;
+  p1display.textContent = p1score;
+  p2display.textContent = p2score;
 
 })
