563ed65cd88ead02f266c78d4906adab5c22cc23
ThuglifeScientist2018 <=>
2020-01-19 14:58:26 +0545

    winner updates

--- js/index.js
+++ js/index.js
@@ -10,8 +10,9 @@ var p2score = 0;
 var gameover = false;
 var winscore = 5;
 var icons = ["fa fa-desktop", "fa fa-tablet"];
-var coinEvents = ["click", "dblclick"];
+var coinEvents = ["click", "dblclick"]; //events for pc
 
+//pc version;
 
 for(i=0; i<coinEvents.length; i++){
 coin.addEventListener(coinEvents[i], function(event){
@@ -35,7 +36,7 @@ coin.addEventListener(coinEvents[i], function(event){
       document.body.appendChild(x);
       var congrats = document.createElement("h1");
       congrats.setAttribute("class", "congrats");
-      congrats.innerHTML = "Congrats <i class='fa fa-desktop'></i>" ;
+      congrats.innerHTML = "Congrats <pre></pre><pre></pre> <i class='fa fa-desktop'></i>" ;
       var xel = document.querySelector(".splashscreen");
       xel.appendChild(congrats);
       gameover = true;
@@ -50,7 +51,7 @@ coin.addEventListener(coinEvents[i], function(event){
       document.body.appendChild(x);
       var congrats = document.createElement("h1");
       congrats.setAttribute("class", "congrats");
-      congrats.innerHTML = "Congrats   <i class='fa fa-tablet'></i>" ;
+      congrats.innerHTML = "Congrats <pre></pre><i class='fa fa-tablet'></i>" ;
       var xel = document.querySelector(".splashscreen");
       xel.appendChild(congrats);
       gameover = true;
@@ -66,3 +67,61 @@ resetButton.addEventListener("click", function(){
   coinIcon.removeAttribute("class");
   gameover = false;
 })
+
+  //android version
+  // for(i=0; i<coinEvents.length; i++){
+  // coin.addEventListener(coinEvents[i], function(event){
+  // if(!gameover){
+  // if(event.type === "click") {
+  // this.style.animationName = 'toss';
+  // coinIcon.setAttribute("class", "fa fa-chevron-down");
+  // coinIcon.style.animationName = "toss";
+  //
+  // }else if(event.type = "dblclick") {
+  // var randomIcon = Math.floor(Math.random()*icons.length);
+  // this.style.animationName = "";
+  // coinIcon.setAttribute("class", icons[randomIcon]);
+  // if(coinIcon.getAttribute("class") === icons[0]){
+  //   p1score ++;
+  //   p1display.textContent = p1score;
+  //   if(p1score === winscore){
+  //     var x = document.createElement("div");
+  //     x.setAttribute("class", "splashscreen")
+  //     x.classList.add("splashscreen");
+  //     document.body.appendChild(x);
+  //     var congrats = document.createElement("h1");
+  //     congrats.setAttribute("class", "congrats");
+  //     congrats.innerHTML = "Congrats <i class='fa fa-desktop'></i>" ;
+  //     var xel = document.querySelector(".splashscreen");
+  //     xel.appendChild(congrats);
+  //     gameover = true;
+  //   }
+  // }else if(coinIcon.getAttribute("class") === icons[1]){
+  //   p2score ++;
+  //   p2display.textContent = p2score;
+  //   if(p2score === winscore){
+  //     var x = document.createElement("div");
+  //     x.setAttribute("class", "splashscreen")
+  //     x.classList.add("splashscreen");
+  //     document.body.appendChild(x);
+  //     var congrats = document.createElement("h1");
+  //     congrats.setAttribute("class", "congrats");
+  //     congrats.innerHTML = "Congrats   <i class='fa fa-tablet'></i>" ;
+  //     var xel = document.querySelector(".splashscreen");
+  //     xel.appendChild(congrats);
+  //     gameover = true;
+  //   }
+  // }
+  // }
+  // }})};
+  // resetButton.addEventListener("touchstart", function(event){
+  // if(event.touchend){
+  // p1score = 0;
+  // p2score = 0;
+  // p1display.textContent = p1score;
+  // p2display.textContent = p2score;
+  // coinIcon.removeAttribute("class");
+  // coin.style.animationName = "";
+  // gameover = false;
+  // }
+  // })
